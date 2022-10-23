a4aec3cf5a61153118bad34df4364003f7035979
Berigoo <muhamadrizqifajri2@gmail.com>
2022-10-23 14:08:55 +0700

    Mengubah tugas3_3

--- tugas3_3.cpp
+++ tugas3_3.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+using namespace std;
 int main(){
-  float inI=10, inV=8, outI, outV, outR;  // input hanya I dan V
-  outV = (outR = inV/inI) * (outI = inV/outR); // urutan perkalian tidak boleh dibalik
-  std::cout << "Nilai arus (I) = " << outI << "\nNilai tegangan (V) = " << outV << "\nNilai hambatan (R) = " << outR << '\n';
+  float i, v, r;
+  cout << "Masukan Nilai arus dan tegangan: "; // Hambatan = tegangan / arus
+  cin >> i >> v;
+  (cin) ? cout<< "Nilai Hambatan = " << v / i << "\nMasukan nilai hambatan dan tegangan: " : cout << "Error mennghitung nilai Hambatan\n";  // cin akan return true jika yang diinput adalah nilai numeric dan false jika non-numeric
+  
+  cin >> r >> v;                              // Arus = tegangan / hambatan
+  (cin) ? cout<< "Nilai Arus = " << v / r << "\nMasukan nilai arus dan hambatan: " : cout << "Error mennghitung nilai Arus\n";
+
+  cin >> i >> r;                              // Tegangan = arus * hambatan  
+  (cin) ? cout<< "Nilai Tegangan = " << i * r << '\n' : cout << "Error mennghitung nilai Tegangan\n";
+  
 }
